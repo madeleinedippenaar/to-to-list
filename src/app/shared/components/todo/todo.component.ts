3b5394fcfd7003af1d7fc3a9b8dc6d7546a63bb9
madeleinedippenaar <madeleinedippenaar0@gmail.com>
2021-01-15 18:53:08 -0500

    first commit

--- src/app/shared/components/todo/todo.component.ts
+++ src/app/shared/components/todo/todo.component.ts
@@ -8,23 +8,48 @@ import { Todo } from '../../models/todo';
 })
 export class TodoComponent implements OnInit {
 
- todos: Todo[] = [];
+  public todos: Todo[] = [
+    {completed: false, task: 'fold clothes'},
+    {completed: false, task: 'put clothes in dresser'},
+    {completed: false, task: 'relax'},
+    {completed: true, task: 'try to pet cat'},
+    {completed: false, task: 'pet dog'},
+    {completed: false, task: 'be awesome'},
+  ];
+
+  filteredTodos: Todo[] = [];
+
+  taskName: string = '';
+  filterValue: string = '';
+  
 
   constructor() { }
 
   ngOnInit(): void {
-    this.todos = [
-      {completed: false, task: 'fold clothes'},
-      {completed: false, task: 'put clothes in dresser'},
-      {completed: false, task: 'relax'},
-      {completed: true, task: 'try to pet cat'},
-      {completed: false, task: 'pet dog'},
-      {completed: false, task: 'be awesome'}
-    ];
+    this.filteredTodos = this.todos;
+  }
+
+  filterTodos(): void {
+    console.log(this.filterValue);
+    this.filteredTodos = this.todos.filter(todo => todo.task.includes(this.filterValue));
   }
 
   completeClicked(todoItem: Todo) {
     todoItem.completed = true;
+  }
+
+  deleteTodo(i: number): void {
+    this.todos.splice(i,1)
+    this.filteredTodos = this.todos;
+    this.filterValue = '';
+  }
+  addTodo () {
+    const todoItem: Todo = {
+      task: this.taskName,
+      completed: false
+    }
+    this.todos.push(todoItem);
+    this.taskName = '';
   }
 
 }
